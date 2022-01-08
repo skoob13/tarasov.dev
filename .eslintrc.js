78becd78d0c635d51d0fd9fc0de6566ed86b2b0d
Georgiy Tarasov <skoob17@mail.ru>
2022-01-08 13:35:05 +0300

    Put ESLint and Prettier in repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+const folders = fs
+  .readdirSync('src', { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
 module.exports = {
-  extends: '@ridgebox/eslint-config',
+  extends: ['plugin:prettier/recommended', 'next/core-web-vitals'],
+  plugins: ['simple-import-sort'],
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts', '*.js', '*.jsx', '*.mjs'],
+      rules: {
+        'react/display-name': 'off',
+        'simple-import-sort/imports': [
+          'warn',
+          {
+            groups: [['^\\u0000', '^react$', '^@?\\w'], [`^(${folders.join('|')})(/.*|$)`], ['^\\.'], ['^[^.]']].filter(
+              (rule) => rule
+            ),
+          },
+        ],
+      },
+    },
+  ],
 };
